214a2ac978b32e1a5a133d604b5ad56fdc075a38
Robert Richey <rob@rocketnext.com>
2013-06-22 20:46:36 -0600

    Adds initial rails framework

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MakeItCaine::Application.config.secret_token = 'b58cd55a428c677e0b54d2539fef7f1decf31490e83508d863a18a5dbc0d9b02a4c1d589d888bbae0523aee71804d27b7942d779f85acbaceab7703f3feb3801'
